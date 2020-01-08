33c05661bd7120cde8cfb1c75d43854c18f67f6c
MikeBeller <bellermike@gmail.com>
2020-01-08 13:52:39 -0500

    Modify client to support multiple requests

--- simplehttp/src/bin/client.rs
+++ simplehttp/src/bin/client.rs
@@ -1,13 +1,21 @@
 use ureq;
 
 fn main() {
-    let resp = ureq::get("http://127.0.0.1:7878/hello")
-        .set("X-My-Header", "Secret")
-        .call();
+    let args:Vec<String> = std::env::args().collect();
+    let nreqs = args[1].parse::<i32>().unwrap();
 
-    if resp.ok() {
-        println!("OK: {}", resp.into_string().unwrap());
-    } else {
-        println!("ERR: {}", resp.into_string().unwrap());
+    let mut total_bytes = 0;
+    for _i in 0..nreqs {
+        let resp = ureq::get("http://127.0.0.1:7878/hello")
+            .set("X-My-Header", "Secret")
+            .call();
+
+        if resp.ok() {
+            total_bytes += resp.into_string().unwrap().len();
+        } else {
+            println!("ERR: {}", resp.into_string().unwrap());
+        }
     }
+
+    println!("Total: {}", total_bytes);
 }
